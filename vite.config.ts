e2b7af6b1aedeb72a53f3f7a017ff72097ae622a
elonehoo <hi@elonehoo.me>
2023-02-02 21:29:20 +0800

    fix: update build in tsup

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@
 
 import path from 'path'
 import { defineConfig } from 'vite'
-import types from '@elonehoo/vite-plugin-type-ts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,24 +9,5 @@ export default defineConfig({
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
     },
-  },
-  plugins: [
-    types(),
-  ],
-  build: {
-    outDir: 'dist',
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'index',
-      fileName: 'index',
-    },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-  },
+  }
 })
