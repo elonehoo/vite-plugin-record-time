853016c30aeb2a9c4da9f2a0367f5eeb49dd08f6
elonehoo <hi@elonehoo.me>
2023-03-10 11:05:13 +0800

    fix: update all non-major dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@
 
 import path from 'path'
 import { defineConfig } from 'vite'
+import RecordTime from './src'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins:[
+    RecordTime()
+  ],
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
